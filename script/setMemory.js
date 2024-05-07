830bf6257f7bac7d9b87c555178ec5d63264e5ec
EliseLei <eliseleininger@gmail.com>
2024-05-07 22:44:45 +0200

    init

---
+++ script/setMemory.js
@@ -0,0 +1,55 @@
+let counter = 0;
+let flippedCards = [];
+
+function setMemory() {
+    const paire = $('.paire');
+    paire.each(function(index) {
+        $(this).addClass(`paire${index+1}`);
+    });
+
+    paire.click(function(event){
+        event.preventDefault();
+        $(this).children(".content").removeClass('hidden');
+        if(counter <= 2) {
+            flippedCards.push($(this));
+            console.log(counter)
+            counter++;
+        }
+
+        if(counter === 2) {
+            const classNames = flippedCards.map(card => card.attr('class').split(' ')[1]);
+            if(classNames[0] === classNames[1]){
+                flippedCards[0].addClass('bg-green-700');
+                flippedCards[1].addClass('bg-green-700');
+            }
+            else {
+                console.log("the card selected")
+                flippedCards.forEach(function(card) {
+                    card.children(".content").removeClass('hidden');
+                });
+                
+                setTimeout(function() {
+                    paire.each(function(index) {
+                        $(this).children(".content").addClass('hidden');
+                    });
+            }, 1000);
+            }
+            flippedCards = [];
+            counter = 0;
+        }
+    });
+}
+
+
+function shuffleCards() {
+    const container = $('.f-full');
+    const cards = container.children('.paire');
+    for (let i = cards.length; i >= 0; i--) {
+        container.append(cards.eq(Math.floor(Math.random() * i)));
+    }
+}
+
+$(document).ready(function(){
+    shuffleCards();
+    setMemory();
+});
